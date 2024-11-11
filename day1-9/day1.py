f072ad132919f02e5e91fd48346cfd865a426a28
Victor Castro Silva <victorcastrosdev@gmail.com>
2024-11-11 19:10:57 -0300

    day1

---
+++ day1-9/day1.py
@@ -0,0 +1,36 @@
+'''
+    EXERCISE 1
+
+    print("""
+        1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.
+        2. Knead the dough for 10 minutes.
+        3. Add 3g of Salt.
+        4. Leave to rise for 2 hours.
+        5. Bake at 200 degrees C for 30 minutes.""")
+'''
+
+'''
+    EXERCISE 2
+
+    print("Notes from Day 1")
+    print("The print statement is used to output strings")
+    print("Strings are strings of characters")
+    print("String Concatenation is done with the + sign")
+    print("New lines can be created with a \ and the letter n")
+
+    '''
+
+''' 
+    EXERCISE 3
+    glass1 = "milk"
+    glass2 = "juice"
+    glass1, glass2 = glass2, glass1
+
+
+'''
+
+glass1 = "milk"
+glass2 = "juice"
+glass1, glass2 = glass2, glass1
+
+print(glass1, glass2)
